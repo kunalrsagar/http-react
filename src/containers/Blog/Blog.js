e5da64451c99dbc53be1fb601192820daaa9745c
Kunal <sagar.kunal@yahoo.com>
2020-04-13 18:58:16 +0530

    Use Link compoenent to avoid re-rendering of page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
@@ -13,8 +13,8 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
